a7c315166c21ad20ffbf81f68ba96bb9d2e91603
zcord <zeus.cordeiro@gmail.com>
2023-02-20 12:52:39 -0500

    Finished routes for API

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Blog, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
 
@@ -33,12 +34,32 @@ router.get('/blog/:id', async (req, res) => {
             }]
         });
         const blog = blogData.get({ plain: true });
-        console.log(blog);
+
         res.render('blog', blog);
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Blog }]
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+          ...user,
+            logged_in: true
+        });
+       
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 router.get('/login', (req, res) => {
